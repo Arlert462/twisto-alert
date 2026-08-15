3aa85fbe5e641d84eb8964c386767a5f1811c8c9
Arlert462 <arlert@tutamail.com>
2026-08-15 14:54:41 +0200

    Ajout du déploiement GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/twisto-alert/',
   optimizeDeps: {
     exclude: ['maplibre-gl'],
   },
